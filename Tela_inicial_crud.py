d4df6e349fa980c2099689da477879a006543f3f
Fhelipe Ribeiro <saselegendas@gmail.com>
2025-09-09 13:48:33 -0300

    Feat: painel de opcoes criado

--- Tela_inicial_crud.py
+++ Tela_inicial_crud.py
@@ -1,5 +1,22 @@
 def tela_inicial():
-    print("Olá, bem vindo ao sistema de gerenciamento do banco SIS!")
-    print("Você gostaria de acessar qual funcionalidade?\n(1) Solicitar cartão\n(2) Gerenciar usuário (3)\n(4) Configurar chave(PIX)")
+    print("Olá, bem vindo ao sistema de gerenciamento do banco SIS!\n\n")
+    print("Você gostaria de acessar qual funcionalidade?\n(1) Solicitar cartão\n(2) Gerenciar usuário\n(3) Configurar chave(PIX)\n")
+    painel()
+
+def painel():
+    selecao = int(input("Inserir funcionalidade, apenas digite o número indicado.\n"))
+    if selecao == 1:
+        print("Você escolheu a solicitação de cartão!\nEm breve iremos te enviar a data de entrega do mesmo.")
+        cartao = 'Mastercard'
+        print(f'Seu cartão será bandeira {cartao}. ')
+    elif selecao == 2:
+        print("Você escolheu o gerenciamento de usuário!\nO que você deseja alterar?\n(1) Nome\n(2) Endereço")
+        funcao = int(input())
+        if funcao == 1:
+            nome = input("Insira o nome: ")
+            print(f'O nome atualizado é: {nome}')
+        elif funcao == 2:
+            endereco = input("Insira o endereço: ")
+            print(f'O endereço atualizado é: {endereco}')
 
 tela_inicial()
