b86f5ab38a32217470dcfb6c0d9bdcdc1858c5d5
Fhelipe Ribeiro <saselegendas@gmail.com>
2025-09-09 13:53:15 -0300

    Feat: painel de opcoes ajustado (2)

--- Tela_inicial_crud.py
+++ Tela_inicial_crud.py
@@ -21,11 +21,11 @@ def painel():
     elif selecao == 3:
         funcao = int(input(("Qual chave você gostaria de configurar? (1) CPF \n(2) Email\n")))
         if funcao == 1:
-            cpf = input("Insira seu CPF.")
-            print(f'Sua chave (CPF) = {cpf}')
+            cpf = input("Insira seu CPF.\n")
+            print(f'Sua chave (CPF) = {cpf}\n')
         elif funcao == 2:
-            email = input("Insira seu e-mail: ")
-            print(f'Sua chave (E-mail) = {email}')
+            email = input("Insira seu e-mail: \n")
+            print(f'Sua chave (E-mail) = {email}\n')
     else:
         print("Insira uma opção válida.")
 
