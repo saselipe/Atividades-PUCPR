32bc9eed6a449a82efdf20f105c9577b98ba05de
Fhelipe Ribeiro <saselegendas@gmail.com>
2025-09-09 13:38:25 -0300

    feat: Tela inicial gerenciamento bancário

---
+++ Tela_inicial_crud.py
@@ -0,0 +1,5 @@
+def tela_inicial():
+    print("Olá, bem vindo ao sistema de gerenciamento do banco SIS!")
+    print("Você gostaria de acessar qual funcionalidade?\n(1) Solicitar cartão\n(2) Gerenciar usuário (3)\n(4) Configurar chave(PIX)")
+
+tela_inicial()
